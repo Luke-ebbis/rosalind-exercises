2668acd7071c6c6ea4015fba6b4e321bb9018bc3
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-01-29 21:11:43 +0100

    refactored the code to be more modular

--- fib/fib.jl
+++ fib/fib.jl
@@ -1,28 +1,56 @@
 #! /usr/bin/env julia
 #= Solution for the Rabbits and Recurrence Relations problem
 
+usage Comand line usage is as follows.
+    ./fib.jl <n> <k>
 description --- The rosalind problem https://rosalind.info/problems/fib/
 =#
 
-input_lines = readlines()
-input_string = split(input_lines[1], " ")
-arguments = map((x) -> parse(Int, x),
-                input_string)
-n, k = arguments
 
-population = [1, 0]
-current_month = 1
-while current_month < n
+function recursive_rabbits_population(;
+    n::Int,
+    k::Int)
+    #=Calculating the population of recursive rabbits.
 
-    global population[2] = population[1] + population[2] * k
-    current_population = population[2] 
-    pushfirst!(population, current_population)
+    :n: int: The number of generations.
+    :k: int: The breeding rate of each rabbit pair.
+    :return: int: The number of rabbits after n generations with a breeding
+        rate of k.
+    =#
 
-    # Going to the next month
-    global current_month = current_month + 1
-
-    # print(current_month, ' ', population, ' ', k, "\n")
-    
+    population = [1, 0]
+    global current_month = 1
+    while current_month < n
+        global population[2] = population[1] + population[2] * k
+        current_population = population[2] 
+        pushfirst!(population, current_population)
+        # Going to the next month
+        global current_month = current_month + 1
+        # print(current_month, ' ', population, ' ', k, "\n")
+    end
+    return first(population)
 end
 
-print(first(population))
+function main()
+    #= The main procedure
+    :return: To standard out.
+    =#
+    if length(ARGS) != 2
+        println("usage: $PROGRAM_FILE  <n> <k> \n"*
+                "n \t The number of generations. \n"*
+                "k \t The breeding rate of the recursive rabbits.")
+        exit(1)
+    end
+
+    input_strings = ARGS
+    n, k = map((x) -> parse(Int, x), input_strings)
+
+    population = recursive_rabbits_population(n = n, k = k)
+    println(population)
+    exit(0)
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
