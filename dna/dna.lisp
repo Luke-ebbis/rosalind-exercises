f8cbd6fedc7588671f17d7dca0823c4accf8a7f9
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-10-17 19:11:55 +0200

    Basic DNA object.

--- dna/dna.lisp
+++ dna/dna.lisp
@@ -1,13 +1,13 @@
-(defclass book ()
-  ((title :reader book-title
-          :initarg :title)
-   (author :reader book-author
-           :initarg :author))
-  (:documentation "Describes a book."))
-(defmethod print-object ((obj book) out)
-  (print-unreadable-object (obj out :type t)
-    (format out "~s" (book-title obj))))
+; DNA object
+(defclass dna ()
+  ((sequence :reader dna-sequence
+            :initarg :seq))
+  (:documentation "Describes a sequence of DNA."))
 
-(print (make-instance 'book
-               :title "ANSI Common Lisp"
-               :author "Paul Graham"))
+(defmethod print-object ((obj dna) out)
+  (print-unreadable-object (obj out :type t)
+    (format out "~s" (dna-sequence obj))))
+
+(print
+ (make-instance 'dna
+  :seq "AAATTGAG"))
