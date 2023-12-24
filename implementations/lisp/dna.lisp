3307728e8c3697547f495a831e73d348ef417e15
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-12-24 13:34:54 +0100

    finish lisp work

--- implementations/lisp/dna.lisp
+++ implementations/lisp/dna.lisp
@@ -1,10 +1,9 @@
 #!/usr/bin/env -S sbcl --dynamic-space-size 9000 --script
 ;; command line argument
-(defvar file-to-read (nth 1 sb-ext:*posix-argv*))
 
-
-(defun parse-file-as-string (file-path)
-  "Read the contents of the file at the specified path and return it as a string."
+;;;; Parse a file contents to string.
+(defun parse-file (file-path)
+  "Read the contents of the file at the specified path and return as string."
   (with-open-file (stream file-path
                           :direction :input
                           :element-type 'character
@@ -15,15 +14,17 @@
           contents)
         (error "Failed to open file: ~A" file-path))))
 
-(defun break-to-list (str)
+;;; Make a list from a string.
+(defun break-to-list (str) 
 	(coerce str 'list))
 
-;;; https://stackoverflow.com/a/59079382/15753558 recursive.
+;;; https://stackoverflow.com/a/59079382/15753558 recursive method to remove 
+;;; elements from an list..
 (defun remove-element (symbol list replaced)
   (if (null list)
       (reverse replaced)
     (if (eql (car list) symbol)
-        (remove-element symbol (cdr list) (cons nil         replaced)) 
+        (remove-element symbol (cdr list) (cons nil replaced)) 
         (remove-element symbol (cdr list) (cons (car list) replaced)))))
 
 (defun set-from-list (lst)
@@ -32,18 +33,26 @@
             do
             (if (not (member ele st))
                 (push ele st))) st))
-(defun count-in-list (lst)
-		(let ((vec '()))
-		(let ((st (set-from-list lst)))
-				(loop for item in st
-						do
-						(push (count item lst) vec )))
-		(nreverse vec)))
 
+(defun count-in-list (lst st)
+  (let ((vec '()))
+    (dolist (item st)
+      (push (count item lst) vec))
+    (nreverse vec)))
 
-(defvar input-data 
-(remove nil	(remove-element '#\Newline (break-to-list (parse-file-as-string 
-																							file-to-read)) () )) )
-(format t "~{~a~^ ~}"  (count-in-list input-data))
+(defun show-output (output)
+	(format t "~{~a~^ ~}~%" output))
 
+(defun main ()
+	(let* ((file-to-read (nth 1 sb-ext:*posix-argv*))
+				 (input (break-to-list (parse-file file-to-read)))
+				 (sequence-list (remove nil 
+																(remove-element '#\Newline input nil)))
+				 (dna-set '(#\A #\C #\G #\T))
+				 (freq (count-in-list sequence-list dna-set)))
+  (show-output freq))	)
 
+(main)
+
+;; Writing this code to a binary...
+(sb-ext:save-lisp-and-die "dna-bin" :executable t :toplevel 'main)
